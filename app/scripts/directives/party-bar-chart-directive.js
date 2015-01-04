96137bdd3e510d2b3ff396e669a3a70424d6ebff
Rubysoft <uriklar@gmail.com>
2015-01-04 20:56:20 +0200

    some more party page styling

--- app/scripts/directives/party-bar-chart-directive.js
+++ app/scripts/directives/party-bar-chart-directive.js
@@ -1,35 +1,66 @@
 'use strict';
 
 angular.module('electionPollsApp')
-  .directive('partyBarChart', [function() {
+  .directive('partyBarChart', ['d3Service',function(d3Service) {
     return {
       restrict: 'EA',
       link: function(scope, element, attrs) {
-        var data = _.sortBy(scope.partyResults.results.slice(0,10), function(r) {
-          return Date.parse(r.poll.date);
+        d3Service.d3().then(function(d3) {
+          var data = _.sortBy(scope.partyResults.results.slice(0,10), function(r) {
+            return Date.parse(r.poll.date);
+          });
+          var chart = c3.generate({
+              bindto: '#party-chart-container',
+              data: {
+                columns: [
+                  [scope.partyResults.name].concat(_.map(data,"mandates"))
+                ],
+                type: 'bar',
+                color: function (color, d) { return scope.partyResults.color; },
+                labels: true
+              },
+              bar: {
+                width: {
+                  ratio: 0.5 // this makes bar width 50% of length between ticks
+                }
+              },
+              axis: {
+                x: {
+                  type: 'category',
+                  categories: _.map(data,function(r){
+                    return r.poll.date;// + "("+r.poll.source+")";
+                  })
+                },
+                y: {
+                  show: false,
+                }
+              },
+              grid: {
+                y: {
+                  show: true
+                }
+              },
+              legend: {
+                show: false
+              },
+              padding: {
+                left: 20,
+                right: 20,
+                top: 30
+              },
+              onresized: repositionLabels
+          });
+          
+          setTimeout(function() {
+            repositionLabels();
+          },400);
         });
-        var chart = c3.generate({
-            bindto: '#party-chart-container',
-            data: {
-              columns: [
-                [scope.partyResults.name].concat(_.map(data,"mandates"))
-              ],
-              type: 'bar'
-            },
-            bar: {
-              width: {
-                ratio: 0.5 // this makes bar width 50% of length between ticks
-              }
-            },
-            axis: {
-              x: {
-                type: 'category',
-                categories: _.map(data,function(r){
-                  return r.poll.date + "("+r.poll.source+")";
-                })
-              }
-            }
-        });
+
+        var repositionLabels = function() {
+          setTimeout(function(){
+            d3.selectAll('.c3-text').attr('y',250);
+          },0) 
+        }
       }
     };
   }]);
