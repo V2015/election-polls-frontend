d2940e80e358cb9b4decb8531abd086b5a53d442
Yuval Bar-On <hyuvii@gmail.com>
2015-01-18 17:03:02 +0000

    added splash screen (unfunctional as of yet) and more minor changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,6 +22,20 @@ angular
   .config(function ($routeProvider,RestangularProvider) {
     $routeProvider
       .when('/', {
+        templateUrl: 'views/splash.html',
+        controller: 'SplashController',
+        resolve: {
+          information: function(Restangular) {
+            return {
+              "msg": {
+                "title": "גוש השמאל מתחזק",
+                "text":  "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+              }
+            };
+          }
+        }
+      })
+      .when('/parties', {
         templateUrl: 'views/main.html',
         controller: 'MainController',
         resolve: {
