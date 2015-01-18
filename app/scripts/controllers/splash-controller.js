f7ff5109e1ecc554dd01889a6c62c590bd969233
Yuval Bar-On <hyuvii@gmail.com>
2015-01-18 19:05:42 +0000

    added splash files

---
+++ app/scripts/controllers/splash-controller.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name electionPollsApp.controller:SplashCtrl
+ * @description
+ * # SplashCtrl
+ * Controller of the electionPollsApp
+ */
+angular.module('electionPollsApp')
+  .controller('SplashController', function ($scope,$location,information) {
+    $scope.msg = information.msg;
+    $scope.go = function(path, view) {
+      $scope.pollView = view;
+	  $location.path(path);
+	};
+
+});
