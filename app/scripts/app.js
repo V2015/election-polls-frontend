9020cfb2a54e1eb3b1c1dd0d9ccfdaacf6ff0739
Yo'av Moshe <bjesus@gmail.com>
2015-01-19 20:58:27 +0200

    fixing pages

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,15 +29,28 @@ angular
         controller: 'SplashController',
         resolve: {
           information: function(Restangular) {
-            return {
-              "msg": {
-                "title": "גוש השמאל מתחזק",
-                "text":  "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-              }
-            };
+            return Restangular.oneUrl('polls-summary.json', 'http://mail.v15.org.il/storage/polls-summary.json').get().then(function (data) {
+                    return data;
+                  }, function () {
+                    return []; // failure
+                  });
+          },
+          pollData: function(Restangular){
+            return Restangular.all('polls.json').getList().then(function (data) {
+                    return data;
+                  }, function () {
+                    return []; // failure
+                  });
+          },
+          partyData: function(Restangular){
+            return Restangular.all('parties.json').getList().then(function (data) {
+              return data;
+            }, function () {
+              return []; // failure
+            });
           }
         }
-      })
+      })   // http://mail.v15.org.il/storage/polls-summary.txt
       .when('/parties', {
         templateUrl: 'views/main.html',
         controller: 'MainController',
--- app/scripts/controllers/splash-controller.js
+++ app/scripts/controllers/splash-controller.js
@@ -8,8 +8,8 @@
  * Controller of the electionPollsApp
  */
 angular.module('electionPollsApp')
-  .controller('SplashController', function ($scope,$location,information,pollView) {
-    $scope.msg = information.msg;
+  .controller('SplashController', function ($scope, $sce, $location,information,pollView) {
+    $scope.msg = $sce.trustAsHtml( information.msg.text );
     $scope.go = function(path, view) {
     	pollView.view = view;
 	  	$location.path(path);
